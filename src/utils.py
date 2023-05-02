ac7b32b7ec93f8ab431f20547e631703aaec398c
WRXer <king_311@mail.ru>
2023-05-02 20:48:33 +0400

    аписал функцию для парсинга по нескольким организациям

--- src/utils.py
+++ src/utils.py
@@ -7,18 +7,13 @@ from src.engine_classes import HH
 
 
 
-
-
-
-
-
-
-
-
-
-
 def main():
-    s_q = 'Точка'
-    hh = HH(s_q)  # hh = HH(search_query) ИНИТИ Точка Softline Predicto Skyeng Технопром Автомакон Idaproject МВП Цифровые решения
-    hh_vacancies = hh.get_request()
-    print(hh_vacancies)
+    hh_vacancies = []
+    s_q = ['ИНИТИ', 'Точка', 'Softline', 'Predicto', 'Skyeng', 'Технопром', 'Автомакон', 'ГоИНВЕСТ', 'МВП', 'ПРОМФИНСТРОЙ']
+    for s in s_q:
+        hh = HH(s)  # hh = HH(search_query) ИНИТИ Точка Softline Predicto Skyeng Технопром Автомакон ГоИНВЕСТ МВП ПРОМФИНСТРОЙ
+        hh_vac = hh.get_request()
+        hh_vacancies = hh_vacancies + hh_vac
+    for v in hh_vacancies:
+        if len(v) > 1:
+            print(v)
